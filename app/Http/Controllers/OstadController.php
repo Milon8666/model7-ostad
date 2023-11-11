28f7f723c2a127e21f601e6497a1288bcbbf7ab2
Milon ali <mdmilonali739@gmail.com>
2023-11-11 23:48:09 +0600

    first commit

---
+++ app/Http/Controllers/OstadController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OstadController extends Controller
+{
+    public function sayHi(){
+        return "Hello laravel";
+    }
+}
